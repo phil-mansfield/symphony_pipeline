a7efe6d758779e68c4674e4e000691ca5b02e598
tdacunha <tdacunha@sas.upenn.edu>
2023-02-03 11:20:18 -0800

    testing new function

--- scripts/ICL.py
+++ scripts/ICL.py
@@ -405,7 +405,9 @@ def main():
         VR, xedges, yedges = np.histogram2d(r_host[ok], v_r[ok], [r_bins,v_bins])
         VR_hist += VR[0]
     fig, ax = plt.subplots()
-    ax.imshow(VR_hist, vmin=.0000001)#, aspect = 'equal', extent = (np.min(r_bins), np.max(r_bins), np.min(v_bins), np.max(v_bins)))
+    X, Y = np.meshgrid(xedges, yedges)
+    #ax.imshow(VR_hist, vmin=.0000001)#, aspect = 'equal', extent = (np.min(r_bins), np.max(r_bins), np.min(v_bins), np.max(v_bins)))
+    ax.pcolormesh(X,Y,VR_hist)
     #ax.colorbar()
     fig.savefig(plot_dir+"phasespace.png")
 
